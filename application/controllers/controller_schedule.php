da08429313d97e68187e9f0d8b7a5c93d2564e6f
Dan <dan@d,ru>
2022-04-15 00:09:12 +0700

    Админка через БДху

--- application/controllers/controller_schedule.php
+++ application/controllers/controller_schedule.php
@@ -13,7 +13,7 @@ class Controller_Schedule extends Controller{
 //        var_dump($time_table);
 //        return false; для тестов
         $data['event_data'] = array_reverse($time_table);
-        var_dump(  $time_table );
+       // var_dump(  $time_table );
 //        $data['title'] = 'Портфолио';
         //  $data = $this->model->getNews();
         $this->view->generate('schedule_view.php', 'template_view.php', $data);
