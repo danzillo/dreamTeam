552b89f9b9285fad63f955244675768308b81d76
Dan <dan@d,ru>
2022-05-04 11:58:59 +0700

    ideal login page

--- application/controllers/controller_login.php
+++ application/controllers/controller_login.php
@@ -10,37 +10,35 @@ class Controller_Login extends Controller
 
     function index()
     {
+        //в admin data - данные всех администраторов сайта
         $admin_data = $this->model->get_login_data();
-        var_dump($admin_data);
-        $data['admin_data'] = array_reverse($admin_data);
-        $listLoginPass = $data['admin_data']; //получаем список админов
-        // foreach ($listLoginPass)
-        // return false;
+
+        // $data['admin_data'] = array_reverse($admin_data);
+        // var_dump($admin_data);
 
         //обрабатываем данные от клиента
         if (isset($_POST['login']) && isset($_POST['pass'])) {
+
             //данные с формы
             $login = $_POST['login'];
             $password = $_POST['pass'];
 
-            //перибираем все пароли и логины из базы данных пока не попадется совпадение
-            foreach ($listLoginPass as $new) {
+            //перибираем весь ассоциативный массив БД пока не попадется совпадение
+            foreach ($admin_data as $new) {
                 if ($login == $new['login'] && $password == $new['pass']) {
                     $data["login_status"] = "sucsess"; //если данные введены верно
                     session_start();// создаем сеанс на сервере для админа
-//                $_SESSION['login'] = $_POST['login'];
-//                $_SESSION['pass'] = $_POST['pass'];
                     $_SESSION['status'] = "administrator"; //статус сессии
                     header("Location:/admin/");
-                } else {
-                    $data ["login_status"] = "fail";// если данные введены с ошибкой
+                } else if ($new == end($admin_data)){
+                    // если данные введены с ошибкой и перебраны все возможноые комбинации
+                    $data ["login_status"] = "fail";
                 }
             }
-
-
         } else {
             $data["login_status"] = "";
         }
+
         $this->view->generate("login_view.php", "admin_template_view.php", $data);
     }
 }
