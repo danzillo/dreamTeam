b61aadfc6495bd47071855c7e380d6e13553b3c2
danzillo <dan4ikhom@gmail.com>
2022-05-06 01:02:36 +0700

    adminka for registration 100%

--- application/controllers/controller_main.php
+++ application/controllers/controller_main.php
@@ -4,7 +4,10 @@ class Controller_Main extends Controller
 {
 
 	function index()
-	{	
+	{
+        //выход из сессии админа
+        session_start();
+        session_destroy();
 		$this->view->generate('main_view.php', 'template_view.php');
 	}
 }
