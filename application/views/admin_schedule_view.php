9f70144a1b7099ea072caf5703d3947856aa63a6
danzillo <dan4ikhom@gmail.com>
2022-05-26 18:27:05 +0700

    Добавил форму для ввода коментария

--- application/views/admin_schedule_view.php
+++ application/views/admin_schedule_view.php
@@ -42,56 +42,58 @@
 
 
 <h>Общий список</h>
+<table class="iksweb">
+    <tbody>
+    <tr>
+        <th>День</th>
+        <th>Описание</th>
+        <th>Время</th>
+
+    </tr>
+    <?php
+    $all_list = $data['all_events'];
+    foreach ($all_list
+
+    as $list){
+    ?>
+
+    <tr>
+        <td><?= $list['day_name'] ?></td>
+        <td><?= $list['event_theme'] ?></td>
+        <td><?= $list['time_text'] ?></td>
+    </tr>
+    </tbody>
+    <?php
+    }
+    //?>
+
+</table>
+
 <form method="post" action="">
-    <table class="iksweb">
-        <tbody>
-        <tr>
-            <th>День</th>
-            <th>Описание</th>
-            <th>Время</th>
+    <p><select size="1" name="day">
+            <option disabled>Выберите день недели</option>
+            <?php
+            $all_days = $data['all_days'];
+            var_dump($data["all_days"]);
+            foreach ($all_days as $all_day) {
+                ?>
+                <option value="<?= $all_day['day_name'] ?>"><?= $all_day['day_name'] ?></option>
+            <?php } ?>
+        </select></p>
 
-        </tr>
-        <?php
-        $all_list = $data['all_events'];
-        foreach ($all_list
+    <p><select size="1" name="time">
+            <option disabled>Выберите время</option>
+            <?php
+            $all_times = $data["all_times"];
+            var_dump($data["all_times"]);
+            foreach ($all_times as $all_time) {
+                ?>
+                <option value="<?= $all_time['time_text'] ?>"><?= $all_time['time_text'] ?></option>
+            <?php } ?>
+        </select></p>
 
-        as $list){
-        ?>
+    <textarea name="comment" cols="40" rows="3"></textarea></p>
 
-        <tr>
-            <td><?= $list['day_name'] ?></td>
-            <td><?= $list['event_theme'] ?></td>
-            <td><?= $list['time_text'] ?></td>
-        </tr>
-        </tbody>
-        <?php
-        }
-        //?>
+    <input type="submit" value="Добавить в расписание">
 
-    </table>
-
-    <form>
-        <p><select size="1">
-                <option disabled>Выберите день недели</option>
-                <?php
-                $all_days = $data['all_days'];
-                var_dump($data["all_days"]);
-                foreach ($all_days as $all_day) {
-                    ?>
-                    <option value="<?= $all_day['day_name'] ?>"><?= $all_day['day_name'] ?></option>
-                <?php } ?>
-            </select></p>
-    </form>
-
-    <form>
-        <p><select size="1">
-                <option disabled>Выберите время</option>
-                <?php
-                $all_times =   $data["all_times"];
-                var_dump($data["all_times"]);
-                foreach ($all_times as $all_time) {
-                    ?>
-                    <option value="<?= $all_time['time_text'] ?>"><?= $all_time['time_text'] ?></option>
-                <?php } ?>
-            </select></p>
-    </form>
+</form>
