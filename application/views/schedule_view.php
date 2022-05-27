fccfa33b253809d9ba2befdc6d5e7cd517648133
danzillo <dan4ikhom@gmail.com>
2022-05-27 13:33:40 +0700

    сделал админку на фулл

--- application/views/schedule_view.php
+++ application/views/schedule_view.php
@@ -10,7 +10,6 @@
                 <td class="first1">06.09</td>
                 <td class="first1">07.09</td>
                 <td class="first1"></td>
-                <td class="first1"></td>
 
             </tr>
             <tr>
@@ -18,111 +17,102 @@
                 <td class="first">Среда</td>
                 <td class="first">Пятница</td>
                 <td class="first">Воскресенье</td>
-                <td class="first">Примечания</td>
                 <td class="first opa">Время</td>
             </tr>
-            <tr>
+            <!--    <?php
+            /*    $schedule = array( array(" ", " " , " ", " " , " "),
+                    array(" ", " " , " ", " " , " "),
+                    array(" ", " " , " ", " " , " "),
+                    array(" ", " " , " ", " " , " "),
+                    array(" ", " " , " ", " " , " ")
+                );
 
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="vremya nechet">10:00</td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Дворец Культуры, секция 6</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td class="vremya">11:00</td>
-            </tr>
-            <tr>
+            //заполняю этот массив по дате
+            //и вывожу массив
+                $event_data = $data['event_data'];
+                var_dump($event_data);
+                foreach ($event_data as $event) {
+                $count = 1;
+                for ($i = 1; $i < 5; $i++) {
+                if ($event['day_id'] == 1 && $event['time_id'] == 1 && $count == 1) {
+                */ ?>
+    <td class="nechet"><?php /*echo $event['event_theme'];
+        $count++;
+        */ ?> </td> <? /*//вносим данные в таблицу
+    }  else  */ ?>
+    <td class="nechet"></td> <? /*
+    } */ ?>
+    <td class="vremya nechet"></td> </tr> --><? /*
+    }
+    */ ?>
 
-                <td class="nechet"></td>
-                <td class="nechet">Дворец Культуры, секция 7</td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="vremya nechet">12:00</td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Дворец Культуры, секция 6</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td class="vremya">13:00</td>
-            </tr>
-            <tr>
+            <?php
+            $schedule = array(array(" ", " ", " ", " ", " "),
+                array(" ", " ", " ", " ", " "),
+                array(" ", " ", " ", " ", " "),
+                array(" ", " ", " ", " ", " "),
+                array(" ", " ", " ", " ", " ")
+            );
 
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="vremya nechet">14:00</td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>Дворец Культуры, секция 6</td>
-                <td>На входе предъявить пропуск.</td>
-                <td class="vremya">15:00</td>
-            </tr>
-            <tr>
+            //заполняю этот массив по дате
+            //и вывожу массив
+            $event_data = $data['event_data'];
+            var_dump($schedule);
+            foreach ($event_data as $event) {
+                $schedule[$event['day_id']][$event['time_id']] = $event['event_theme'];
+            }
 
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="vremya nechet">16:00</td>
+            for ($b = 0; $b < 4; $b++) {
+                ?>
+                <td class="nechet"><?php echo $schedule[0][$b];
+                    ?> </td> <? //вносим данные в таблицу
+            } ?>
+            <td class="vremya nechet">10:00-12:00</td>
             </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>Дворец Культуры, секция 6</td>
-                <td>На входе предъявить пропуск.</td>
-                <td class="vremya">17:00</td>
-            </tr>
-            <tr>
 
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="vremya nechet">18:00</td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td class="vremya">19:00</td>
-            </tr>
-            <tr>
+            <?php
 
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="nechet"></td>
-                <td class="vremya nechet">20:00</td>
+            for ($b = 0; $b < 4; $b++) {
+                ?>
+                <td><?php echo $schedule[1][$b];
+                    ?> </td> <? //вносим данные в таблицу
+            } ?>
+            <td class="vremya">12:00-14:00</td>
             </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td class="vremya">21:00</td>
+
+
+            <?php
+
+            for ($b = 0; $b < 4; $b++) {
+                ?>
+                <td class="nechet"><?php echo $schedule[2][$b];
+                    ?> </td> <? //вносим данные в таблицу
+            } ?>
+            <td class="vremya nechet">14:00-16:00</td>
             </tr>
+
+
+            <?php
+
+            for ($b = 0; $b < 4; $b++) {
+                ?>
+                <td><?php echo $schedule[3][$b];
+                    ?> </td> <? //вносим данные в таблицу
+            } ?>
+            <td class="vremya">16:00-18:00</td>
+            </tr>
+
+
+            <?php
+
+            for ($b = 0; $b < 4; $b++) {
+                ?>
+                <td class="nechet"><?php echo $schedule[4][$b];
+                    ?> </td> <? //вносим данные в таблицу
+            } ?>
+            <td class="vremya nechet">18:00-20:00</td>
+            </tr>
+
             <tr>
                 <td class="first2" colspan=6>Дополнительно</td>
             </tr>
@@ -163,158 +153,5 @@
 </style>
 
 
-<table class=" _anim-items animnh ">
-    <caption class="logo js1 _anim-items animnh rasp color2 scale">Расписание</caption>
-
-    <tr>
-        <td class="first1">04.09</td>
-        <td class="first1">05.09</td>
-        <td class="first1">06.09</td>
-        <td class="first1">07.09</td>
-        <td class="first1"></td>
-
-    </tr>
-    <tr>
-        <td class="first">Понедельник</td>
-        <td class="first">Среда</td>
-        <td class="first">Пятница</td>
-        <td class="first">Воскресенье</td>
-        <td class="first opa">Время</td>
-    </tr>
-    <!--    <?php
-    /*    $schedule = array( array(" ", " " , " ", " " , " "),
-            array(" ", " " , " ", " " , " "),
-            array(" ", " " , " ", " " , " "),
-            array(" ", " " , " ", " " , " "),
-            array(" ", " " , " ", " " , " ")
-        );
-
-    //заполняю этот массив по дате
-    //и вывожу массив
-        $event_data = $data['event_data'];
-        var_dump($event_data);
-        foreach ($event_data as $event) {
-        $count = 1;
-        for ($i = 1; $i < 5; $i++) {
-        if ($event['day_id'] == 1 && $event['time_id'] == 1 && $count == 1) {
-        */ ?>
-    <td class="nechet"><?php /*echo $event['event_theme'];
-        $count++;
-        */ ?> </td> <? /*//вносим данные в таблицу
-    }  else  */ ?>
-    <td class="nechet"></td> <? /*
-    } */ ?>
-    <td class="vremya nechet"></td> </tr> --><? /*
-    }
-    */ ?>
-
-    <?php
-    $schedule = array(array(" ", " ", " ", " ", " "),
-        array(" ", " ", " ", " ", " "),
-        array(" ", " ", " ", " ", " "),
-        array(" ", " ", " ", " ", " "),
-        array(" ", " ", " ", " ", " ")
-    );
-
-    //заполняю этот массив по дате
-    //и вывожу массив
-    $event_data = $data['event_data'];
-    foreach ($event_data as $event) {
-        $schedule[$event['day_id']][$event['time_id']] = $event['event_theme'];
-    }
-
-    for ($b = 0; $b < 4; $b++) {
-        ?>
-        <td class="nechet"><?php echo $schedule[0][$b];
-            ?> </td> <? //вносим данные в таблицу
-    } ?>
-    <td class="vremya nechet">10:00-12:00</td>
-    </tr>
-
-    <?php
-
-    for ($b = 0; $b < 4; $b++) {
-        ?>
-        <td><?php echo $schedule[1][$b];
-            ?> </td> <? //вносим данные в таблицу
-    } ?>
-    <td class="vremya">12:00-14:00</td>
-    </tr>
 
 
-    <?php
-
-    for ($b = 0; $b < 4; $b++) {
-        ?>
-        <td class="nechet"><?php echo $schedule[2][$b];
-            ?> </td> <? //вносим данные в таблицу
-    } ?>
-    <td class="vremya nechet">14:00-16:00</td>
-    </tr>
-
-
-    <?php
-
-    for ($b = 0; $b < 4; $b++) {
-        ?>
-        <td><?php echo $schedule[3][$b];
-            ?> </td> <? //вносим данные в таблицу
-    } ?>
-    <td class="vremya">16:00-18:00</td>
-    </tr>
-
-
-    <?php
-
-    for ($b = 0; $b < 4; $b++) {
-        ?>
-        <td class="nechet"><?php echo $schedule[4][$b];
-            ?> </td> <? //вносим данные в таблицу
-    } ?>
-    <td class="vremya nechet">18:00-20:00</td>
-    </tr>
-
-    <tr>
-        <td class="first2" colspan=6>Дополнительно</td>
-    </tr>
-    <tr>
-        <td class="first3" colspan=6>Возможны перебои с горячей водой</td>
-    </tr>
-    <tr>
-        <td class="first3next" colspan=6></td>
-    </tr>
-    <tr>
-        <td class="first3next" colspan=6></td>
-    </tr>
-    <tr>
-        <td class="first3next" colspan=6></td>
-    </tr>
-    <tr>
-        <td class="first3next" colspan=6></td>
-    </tr>
-    <tr>
-        <td class="first" colspan=6></td>
-    </tr>
-    <tr>
-        <td class="first" colspan=6></td>
-    </tr>
-    <tr>
-        <td class="first" colspan=6></td>
-    </tr>
-
-</table>
-
-
-<!--//echo  $event_data;
-//foreach($event_data as $new)
-//{
-//echo $new['day_name'];
-//        echo $new['event_name'];
-//        echo $new['event_dicription'];
-//        echo $new['event_data'];
-//if($new['day_id']==1)
-//echo '<tr><td>'.$new['day_name'].'</td><td>'.$new['event_time'].'<td><td>'.$new['event_theme'].'</td><td>'.$new['speaker_name'].'</td></td>';
-//}
-
-// echo $xyu;
--->
